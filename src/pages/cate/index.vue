93ef7cf10fa7b46ffb2064d597e3089457081e5a
xingqi <xingqi109@163.com>
2019-05-18 16:59:54 +0800

    项目初始化

---
+++ src/pages/cate/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    分类 
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
---
+++ src/pages/home/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <!-- 搜索条 -->
+    <div class="search">
+      <div class="search-bar">
+        <icon type='search'/>
+        <span>搜索</span>
+      </div>
+    </div> 
+    <!-- 轮播图 -->
+    <swiper :indicator-dots='indicatorFlag'>
+      <swiper-item :key='index' v-for="(item, index) in imgUrls">
+        <image :src="item" class="slide-image"/>
+      </swiper-item>
+    </swiper>
+
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      imgUrls: [
+        'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+        'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+      ],
+      // 控制显示底部圆点
+      indicatorFlag: true
+    }
+  }
+}
+</script>
+<style scoped>
+.search {
+  background-color: #EB4450;
+  padding: 10px;
+} 
+.search .search-bar {
+  text-align: center;
+  background-color: #fff;
+}
+.search .search-bar icon {
+  vertical-align: middle;
+}
+.slide-image {
+  width: 750rpx;
+}
+</style>
